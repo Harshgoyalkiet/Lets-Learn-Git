f50255701b702c449bcf6f36627278742645f316
Harsh Goyal <harsh.1923it1084@kiet.edu>
2021-09-09 18:50:24 +0530

    adding multiply.c

---
+++ project/multiply.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    int b;
+    a=4;
+    b=4;
+    printf("result=%d*%d",a,b);
+    int result = a*b;
+    printf("\n");
+    printf("%d",result);
+
+}
